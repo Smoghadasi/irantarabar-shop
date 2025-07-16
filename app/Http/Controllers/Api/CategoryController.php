ebc34a6843bc3e6724b1afe60892e8ac6588afd6
Sadra Moghadasi <sadra.19@gmail.com>
2025-07-16 15:41:13 +0330

    add category

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::selete(['name', 'slug', 'icon'])
+            ->where('is_active', 1)
+            ->where('parent_id', 0)
+            ->take(6)
+            ->get();
+        return response()->json($categories, 200);
+    }
+}
