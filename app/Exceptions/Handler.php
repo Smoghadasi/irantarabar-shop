6fd135ca915eb863e8b8b4dbfc22c2996aa1c092
sadra <sadra.19@gmail.com>
2025-08-01 14:50:29 +0330

    Add: traits

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,14 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -23,8 +26,16 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
+        //
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof ModelNotFoundException) {
+            return $this->errorResponse($e->getMessage(), 404);
+        }
         $this->reportable(function (Throwable $e) {
-            //
+            return $this->errorResponse($e->getMessage(), 500);
         });
     }
 }
