771e89a5d51e92b46fc9e9b2c8fd89d1a76d86c1
Sadra Moghadasi <sadra.19@gmail.com>
2025-08-02 15:28:35 +0330

    add product_fleet

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -26,7 +26,7 @@ class ProductController extends ApiController
 
     public function searchProduct(Request $request)
     {
-        return $request->fleet_ids;
+        // return $request->fleet_ids;
         $products = Product::when($request->fleet_ids, function ($query) use ($request) {
             $query->whereHas('fleets', function ($q) use ($request) {
                 $q->whereIn('fleets.id', $request->fleet_ids);
